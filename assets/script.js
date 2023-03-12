2d73860a69657074871fd4abead759e6185c7380
Jonathan Ryndolf <ryndolf2001@gmail.com>
2023-03-12 23:20:50 +0530

    Basic Structure for history built

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+(function () {
+  "use strict";
+
+  on(
+    "click",
+    ".navbar .dropdown > a",
+    function (e) {
+      if (select("#navbar").classList.contains("navbar-mobile")) {
+        e.preventDefault();
+        this.nextElementSibling.classList.toggle("dropdown-active");
+      }
+    },
+    true
+  );
+})();
